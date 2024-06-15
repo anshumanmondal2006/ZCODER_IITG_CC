96c0c21a834ad166f4b2f6910b0b4ed287612a43
TheAdich <adidadhich2003@gmail.com>
2024-06-15 09:28:55 +0530

    saved

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const middleware=require('./middleware/auth');
 dotenv.config();
 const profile = require('./pages/profile/profile');
 const ask = require('./pages/problem/problem')
-app.use(express.json());
+app.use(express.json());    
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
     secret: process.env.TOKEN_SECRET,
